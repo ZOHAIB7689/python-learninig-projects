9dd52ad464a9091a0ace8a7baad2e1a589183932
ZOHAIB7689 <bzohaib754@gmail.com>
2025-02-09 19:04:11 +0500

    this project is completed

--- project_01/youtube_manager.py
+++ project_01/youtube_manager.py
@@ -9,12 +9,17 @@ def load_data():
         return []
         
 def save_data_helper(videos):
-    with open('youtube.txt' 'w') as file:
+    with open('youtube.txt' ,'w') as file:
         json.dump(videos , file)
 
 def list_all_videos(videos):
+   print("\n")
+   print("_"*70)
    for index , video in   enumerate(videos ,start=1):
-       print(f"{index}.")
+       print(f"{index},Video: {video['name']}, Duration: {video['time']} ")
+   print("\n")
+   print("_"*70)
+
 def add_video(videos):
     name = input("Enter video name: ")
     time = input("Enter video time: ")
@@ -22,11 +27,23 @@ def add_video(videos):
     save_data_helper(videos)
 
 def update_video(videos):
-    pass
-
+    list_all_videos(videos)
+    index = int(input("Enter the video number  you want to update"))
+    if 1<= index<= len(videos):
+        name =input("Enter the new name of video")
+        time = input("Enter the new video time")
+        videos[index-1] = {'name': name , 'time': time}
+        save_data_helper(videos)
+    else :
+        print("invalid index selected")
 def delete_video(videos):
-    pass
-
+    list_all_videos(videos)
+    index = int(input("Enter the video number you want to delete"))
+    if 1<= index <= len(videos):
+        del videos[index-1]
+        save_data_helper(videos)
+    else:
+        print("invalid video selected")
 def main():
     videos = load_data() 
     while True:
@@ -36,7 +53,7 @@ def main():
         print("3. Update a youtube video details")
         print("4. Delete a youtube Video ")
         print("5. Exit the app ")
-        choice = input("Enter your Choice")
+        choice = input("Enter your Choice: ")
         print(videos)
 
         match choice:
