bc3da92f9fd32dc9009d8fdb35905b2078227352
ZOHAIB7689 <bzohaib754@gmail.com>
2025-02-28 09:37:48 +0500

    completed growth mindset challenge

--- lg/src/lg/simpleflow.py
+++ lg/src/lg/simpleflow.py
@@ -13,9 +13,9 @@ def task2():
     
 
 
-@entrypoint
+@entrypoint()
 def run_flow(input:str):
-    print("Running Simple Workflow",)
+    print("Running Simple Workflow", input)
     task1_output = task1()
     task2_output = task2()
     return f"workflow executed successfully with outputs {task1_output} and {task2_output}"
@@ -23,3 +23,11 @@ def run_flow(input:str):
 
 def  run_chain():
     run_flow.invoke("Simple Input")
+
+
+
+def check_entrypoint(func):
+    def wraper(input):
+        print("Checking Entry Point")
+        return func(input)
+    return wraper
